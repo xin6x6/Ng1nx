28caff20e62af0c10566d482faef8197b6cd8d85
Ng1nx <91785403+xin6x6@users.noreply.github.com>
2025-08-12 12:40:57 +0800

    Update script.js

--- Notes/script.js
+++ Notes/script.js
@@ -44,7 +44,7 @@ function renderMessages(msgs) {
     const newHTML = msgs.map(m => `
         <div class="message-item">
             <strong>${escapeHTML(m.name)}</strong>
-            <button class="btn-retract" onclick="retractMessage('${m.id}')">撤回</button>
+            <button class="btn-retract" onclick="retractMessage('${m.id}'); longPollMessages() style="right=0px">撤回</button>
             <p>${escapeHTML(m.message)}</p>
             <small>${new Date(m.created_at).toLocaleString()}</small>
         </div>
