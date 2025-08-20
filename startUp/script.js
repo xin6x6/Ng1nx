4f1c314c4bae161189d80d46b84eb5f2df79893d
xin6x6 <91785403+xin6x6@users.noreply.github.com>
2025-08-20 14:47:48 +0800

    add reboot

--- startUp/script.js
+++ startUp/script.js
@@ -1,18 +1,48 @@
 const startingLogo = document.getElementById("startingLogo");
 const barEmpty = document.getElementById("barEmpty");
 const barFull = document.getElementById("barFull");
-setTimeout(() => {
-    startingLogo.style.opacity = "100";
+const startUpAudio = document.getElementById("startUpAudio");
+let random = Math.random() / 3;
+console.log(random);
 
-    setTimeout(() => {
-        barEmpty.style.opacity = "100";
-        setTimeout(() => {
-            barFull.style.opacity = "100";
-            for (let i = 0; i < 100; setTimeout(() => {i++}, 300)){
-                barFull.style.width = (i/100) * 250 + "px";
+function barFill() {
+    barEmpty.style.opacity = "100";
+    setTimeout(() => { //bar fill
+        barFull.style.opacity = "100";
+        for (let i = 0; i < 10000; i++) {
+            setTimeout(() => {
+                barFull.style.width = (i / 10000) * 250 + "px";
+            }, i * random);
+
+            if(i >= 9999) {
+                setTimeout(() => {
+                    window.location.href = "../index.html";
+                }, 2000);
             }
-        }, 1500);
-    }, 100);
+        }
+    }, 1500);
+
+    
+}
+
+setTimeout(() => { //logo delay
+
+
+    startUpAudio.muted = false;
+    try{
+        startUpAudio.play();
+    } catch(err){
+        console.log(err);
+        setTimeout(() => {
+            barFill();
+        }, 2000);
+    }
+    startingLogo.style.opacity = "100";
+    // bar funcs
+    startUpAudio.addEventListener("ended", () => { // show bar when ended audio
+        barFill();
+    });
+
+
 
 }, 1000);
-
