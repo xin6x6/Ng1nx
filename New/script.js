f9d89f2d2522f05dbce17812c9815d77e6cc6f40
xin6x6 <91785403+xin6x6@users.noreply.github.com>
2025-09-21 17:31:10 +0800

    Built suc

--- New/script.js
+++ New/script.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
 
+});
+
+const confirmNotification = document.getElementById('confirmNotification');
+const notification = document.querySelector('.notification-container');
+confirmNotification.addEventListener('click', function(){
+    notification.style.display = 'none';
 })
