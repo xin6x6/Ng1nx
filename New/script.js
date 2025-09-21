51d8d79fca1ce71c34f30b4499abb285766ff2ed
xin6x6 <91785403+xin6x6@users.noreply.github.com>
2025-09-21 18:03:30 +0800

    ppp

--- New/script.js
+++ New/script.js
@@ -1,9 +1,16 @@
-document.addEventListener('DOMContentLoaded', function(){
 
-});
-
+// notification
 const confirmNotification = document.getElementById('confirmNotification');
 const notification = document.querySelector('.notification-container');
 confirmNotification.addEventListener('click', function(){
     notification.style.display = 'none';
 })
+
+//sub-nav
+const subnav = document.querySelectorAll('.sub-nav').forEach(el => {
+    el.addEventListener('click', (e) => {
+        const name = el.dataset.url;
+        const url = "./sub-nav/" + name + "/index.html";
+        window.open(url, "_blank");
+    })
+});
