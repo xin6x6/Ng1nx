5145d43a191c2883b4a951374d73b962b4f48500
xin6x6 <91785403+xin6x6@users.noreply.github.com>
2025-10-10 22:22:22 +0800

    111

---
+++ New/sub-nav/代码图片生成/script.js
@@ -0,0 +1,26 @@
+const textarea = document.getElementById("code");
+const codeArea = document.querySelector(".code-area");
+// import from "./http_cdnjs.cloudflare.com_ajax_libs_highlight.js_9.12.0_highlight.min"
+
+function adjustHeight() {
+    // 重置 textarea 高度
+    textarea.style.height = "auto";
+
+    // 设置 textarea 高度为内容高度
+    textarea.style.height = textarea.scrollHeight + "px";
+
+    // 设置父容器最小高度以适应 textarea
+    codeArea.style.minHeight = textarea.scrollHeight + "px";
+}
+
+// 页面加载时初始化
+adjustHeight();
+
+// 输入时自动调整高度
+textarea.addEventListener("input", adjustHeight);
+
+//
+// const code = document.getElementById("code");
+// code.addEventListener("change", (el) => {
+//     hljs.highlightElement(el);
+// })
